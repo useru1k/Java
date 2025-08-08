c44bed6f05ad9c2d92bce1b0e5d7b48e1bf4b931
Yuvan krishna P <143686903+useru1k@users.noreply.github.com>
2025-08-08 13:49:46 +0530

    Create  4A - Watermelon.java

---
+++ Codeforces/4A - Watermelon.java
@@ -0,0 +1,16 @@
+// https://codeforces.com/problemset/problem/4/A
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n % 2 == 0 && n != 2) {
+            System.out.println("YES");
+        }
+        else {
+            System.out.println("NO");
+        }
+        sc.close();
+    }
+}
