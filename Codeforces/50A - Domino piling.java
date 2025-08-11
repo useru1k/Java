737f881ac219bc2a6483023fa65fe9005881379f
Yuvan krishna P <143686903+useru1k@users.noreply.github.com>
2025-08-11 09:46:13 +0530

    Create 50A - Domino piling.java

---
+++ Codeforces/50A - Domino piling.java
@@ -0,0 +1,12 @@
+// https://codeforces.com/problemset/problem/50/A
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m  = sc.nextInt();
+        System.out.println((n*m)/2);
+        sc.close();
+    }
+}
